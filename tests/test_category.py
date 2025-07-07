3354d6ed61a8deefcdac6992fd677869ecae344a
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-07-07 17:30:22 +0300

    добавлены тесты для проверки category_count и product_count

--- tests/test_category.py
+++ tests/test_category.py
@@ -66,3 +66,33 @@ def test_second_category(second_category):
     assert second_category.products[0].description == "Фоновая подсветка"
     assert second_category.products[0].price == 123000.0
     assert second_category.products[0].quantity == 7
+
+
+def test_category_class_counters():
+    Category.category_count = 0
+    Category.product_count = 0
+
+    cat1 = Category(
+        name="Смартфоны",
+        description=(
+            "Смартфоны, как средство не только коммуникации,"
+            " но и получения дополнительных функций для удобства жизни"
+        ),
+        products=[
+            Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5),
+            Product("Iphone 15", "512GB, Gray space", 210000.0, 8),
+            Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+        ]
+    )
+
+    cat2 = Category(
+        name="Телевизоры",
+        description=(
+            "Современный телевизор, который позволяет наслаждаться просмотром, "
+            "станет вашим другом и помощником"
+        ),
+        products=[Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)]
+    )
+
+    assert Category.category_count == 2
+    assert Category.product_count == 4
