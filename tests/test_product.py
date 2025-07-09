d2f6aa69f2e500c36d9de8f5de414ba864877f85
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-07-09 19:35:42 +0300

    вынесены фикстуры в conftest.py, убраны лишние импорты

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,38 +1,3 @@
-import pytest
-
-from src.product import Product
-
-
-@pytest.fixture
-def first_product():
-    return Product(
-        name="Samsung Galaxy S23 Ultra",
-        description="256GB, Серый цвет, 200MP камера",
-        price=180000.0,
-        quantity=5
-    )
-
-
-@pytest.fixture
-def second_product():
-    return Product(
-        name="Iphone 15",
-        description="512GB, Gray space",
-        price=210000.0,
-        quantity=8
-    )
-
-
-@pytest.fixture
-def third_product():
-    return Product(
-        name="Xiaomi Redmi Note 11",
-        description="1024GB, Синий",
-        price=31000.0,
-        quantity=14
-    )
-
-
 #  Тест с инициализацией первого продукта
 def test_init_first(first_product):
     assert first_product.name == "Samsung Galaxy S23 Ultra"
