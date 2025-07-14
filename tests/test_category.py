e13e55916918507bc3ce5d600cbb163ce04bbfd3
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-07-14 23:35:23 +0300

    изменены тесты в соотвествие с новым геттером из category.py

--- tests/test_category.py
+++ tests/test_category.py
@@ -8,20 +8,20 @@ def test_first_category(first_category):
         "но и получения дополнительных функций для удобства жизни"
     )
 
-    assert first_category.products[0].name == "Samsung Galaxy S23 Ultra"
-    assert first_category.products[0].description == "256GB, Серый цвет, 200MP камера"
-    assert first_category.products[0].price == 180000.0
-    assert first_category.products[0].quantity == 5
+    assert first_category.product_items[0].name == "Samsung Galaxy S23 Ultra"
+    assert first_category.product_items[0].description == "256GB, Серый цвет, 200MP камера"
+    assert first_category.product_items[0].price == 180000.0
+    assert first_category.product_items[0].quantity == 5
 
-    assert first_category.products[1].name == "Iphone 15"
-    assert first_category.products[1].description == "512GB, Gray space"
-    assert first_category.products[1].price == 210000.0
-    assert first_category.products[1].quantity == 8
+    assert first_category.product_items[1].name == "Iphone 15"
+    assert first_category.product_items[1].description == "512GB, Gray space"
+    assert first_category.product_items[1].price == 210000.0
+    assert first_category.product_items[1].quantity == 8
 
-    assert first_category.products[2].name == "Xiaomi Redmi Note 11"
-    assert first_category.products[2].description == "1024GB, Синий"
-    assert first_category.products[2].price == 31000.0
-    assert first_category.products[2].quantity == 14
+    assert first_category.product_items[2].name == "Xiaomi Redmi Note 11"
+    assert first_category.product_items[2].description == "1024GB, Синий"
+    assert first_category.product_items[2].price == 31000.0
+    assert first_category.product_items[2].quantity == 14
 
 
 def test_second_category(second_category):
@@ -31,10 +31,10 @@ def test_second_category(second_category):
         "станет вашим другом и помощником"
     )
 
-    assert second_category.products[0].name == "55\" QLED 4K"
-    assert second_category.products[0].description == "Фоновая подсветка"
-    assert second_category.products[0].price == 123000.0
-    assert second_category.products[0].quantity == 7
+    assert second_category.product_items[0].name == "55\" QLED 4K"
+    assert second_category.product_items[0].description == "Фоновая подсветка"
+    assert second_category.product_items[0].price == 123000.0
+    assert second_category.product_items[0].quantity == 7
 
 
 def test_category_class_counters(first_product, second_product, third_product, fourth_product):
