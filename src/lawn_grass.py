cfe08b8aa4a4df7caa659b240341872fec606717
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-07-28 22:54:14 +0300

    добавлен сетод сложения, наследующий общую логику из Product, но с проверкой принадлежности объектов разных классов

--- src/lawn_grass.py
+++ src/lawn_grass.py
@@ -2,17 +2,25 @@ from src.product import Product
 
 
 class LawnGrass(Product):
+    """Подкатегория класса Product для представления Газонная трава"""
 
-    def __init__(self,
-                 name: str,
-                 description: str,
-                 price: float,
-                 quantity: int,
-                 country: str,
-                 germination_period: str,
-                 color: str
+    def __init__(
+            self,
+            name: str,
+            description: str,
+            price: float,
+            quantity: int,
+            country: str,
+            germination_period: str,
+            color: str
     ):
         super().__init__(name, description, price, quantity)
         self.country = country
         self.germination_period = germination_period
         self.color = color
+
+    def __add__(self, other):
+        if type(other) is type(self):
+            return super().__add__(other)
+        else:
+            raise TypeError("Можно складывать только траву с травой")
