eea59eccaeda3fb8bc70ac298ea837dd02a78eee
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-07-28 22:56:18 +0300

    добавлен метод сложения, наследующий общую логику из Product, но с проверкой принадлежности объектов разных классов

--- src/smartphone.py
+++ src/smartphone.py
@@ -2,19 +2,27 @@ from src.product import Product
 
 
 class Smartphone(Product):
+    """Подкатегория класса Product для представления Смартфонов"""
 
-    def __init__(self,
-                 name: str,
-                 description: str,
-                 price: float,
-                 quantity: int,
-                 efficiency: float,
-                 model: str,
-                 memory: int,
-                 color: str
+    def __init__(
+            self,
+            name: str,
+            description: str,
+            price: float,
+            quantity: int,
+            efficiency: float,
+            model: str,
+            memory: int,
+            color: str
     ):
         super().__init__(name, description, price, quantity)
         self.efficiency = efficiency
         self.model = model
         self.memory = memory
         self.color = color
+
+    def __add__(self, other):
+        if type(other) is type(self):
+            return super().__add__(other)
+        else:
+            raise TypeError("Можно складывать только смартфоны со смартфонами")
